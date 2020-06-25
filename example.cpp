d22e69ddc26c435859eee302f983f0891fe6171e
David Freifeld <freifeld.david@gmail.com>
2020-06-24 19:21:30 -0700

    Forgot to add correct files...

---
+++ example.cpp
@@ -0,0 +1,16 @@
+#include "bpnn.hpp"
+#include "utils.hpp"
+
+int main()
+{
+  Network net ("./extra.txt", 4, 1, 1, 5, 10, 1);
+  net.set_activation(0, "linear");
+  net.set_activation(1, "sigmoid");
+  net.set_activation(2, "resig");
+  //  net.list_net();
+  net.train(50);
+  //  net.list_net();
+  //char line[1024];
+  //net.stream->getline(line, 1024);
+  //std::cout << line << "\n";
+}
---
+++ utils.hpp
@@ -0,0 +1,13 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+double sigmoid(double x);
+double sigmoid_deriv(double x);
+double resig(double x);
+double resig_deriv(double x);
+double relu(double x);
+double relu_deriv(double x);
+double linear(double x);
+double linear_deriv(double x);
+
+#endif /* MODULE_H */
