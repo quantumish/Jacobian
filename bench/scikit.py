7ecdb7cb088fe6c36d890f9bcfbaa63724dd32f2
David Freifeld <freifeld.david@gmail.com>
2020-07-27 14:38:47 -0700

    Updated scikit demo

--- bench/scikit.py
+++ bench/scikit.py
@@ -1,18 +1,34 @@
 from sklearn.neural_network import MLPClassifier
 import csv
+import matplotlib.pyplot as plt
+import time
 
-with open("./data_banknote_authentication.txt", 'rt') as f:
-    reader = csv.reader(f)
-    data = list(reader)
-    for a in data:
-        for b, c in enumerate(a):
-            a[b] = float(a[b])
+def bench(batch_sz):
+    start = time.time()
+    with open("./data_banknote_authentication.txt", 'rt') as f:
+        reader = csv.reader(f)
+        data = list(reader)
+        for a in data:
+            for b, c in enumerate(a):
+                a[b] = float(a[b])
+            
+    X_train = []
+    y_train = []
+    for i in data:
+        X_train.append(i[:-1])
+        y_train.append(i[-1])
 
-X_train = []
-y_train = []
-for i in data:
-    X_train.append(data[:-1])
-    y_train.append(data[-1])
-print(X_train[-1])
+    clf = MLPClassifier(solver="sgd", batch_size=batch_sz)
+    clf.fit(X_train, y_train)
+    end = time.time()
+    return end-start
 
-clf = MLPClassifier(random_state=1, max_iter=300).fit(X_train, y_train)
+# i = 1
+# while (i < 1343):
+#     times = []
+#     times.append(bench(i))
+#     print("Finished loop %s in %s s." %(i, times[-1]))
+#     if (i == 1): i += 9
+#     else: i += 10
+# print(times)
+print(bench(16))    
