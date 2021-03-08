809a45cc2695fa256906fb99ab9368c364922e0a
David Freifeld <freifeld.david@gmail.com>
2021-03-07 19:36:58 -0800

    Beginnings of evaluating MLPs with graph

--- src/graphs.cpp
+++ src/graphs.cpp
@@ -2,8 +2,9 @@
 #include <vector>
 #include <iostream>
 #include <eigen3/Eigen/Dense>
+#include <cmath>
 
-enum class Operation {NONE, add, multiply};
+enum class Operation {NONE, add, multiply, tanh};
 
 struct Node
 {    
@@ -19,6 +20,10 @@ struct Node
 Node::Node(float v) : op(Operation::NONE), val(1,1), args{}, calced(true)
 {
     val(0,0) = v;
+}
+
+namespace Jacobian {
+    Node* tanh(Node* a) {return new Node(Operation::tanh, {a});}
 }
 
 Node::Node(Eigen::MatrixXf v) : op(Operation::NONE), val(v), args{}, calced(true) {}
@@ -44,16 +49,11 @@ Graph::Graph() {}
 
 void Graph::eval(Node* back)
 {
-    std::cout << back->args.size() << "\n";
     for (Node* arg : back->args) {
-	std::cout << "At " << back << ", checking if arg " << arg << " is evaluated..."; 
 	if (arg->calced == false) {
-	    std::cout << "nope!" << "\n";
 	    eval(arg);	    
 	}
-	std::cout << "yup!" << "\n";	
     }
-    std::cout << "Calcing...";
     switch (back->op) {
     case Operation::NONE:
 	break;
@@ -70,24 +70,53 @@ void Graph::eval(Node* back)
 	    back->val*=back->args[i]->val(0,0);
 	}
 	break;
+    case Operation::tanh:
+	for (int i = 0; i < back->val.rows(); i++) {
+            for (int j = 0; j < back->val.cols(); j++) {
+                back->val(i,j) = tanh(back->val(i,j));
+            }
+        }
+	back->val = back->args[0]->val;	
+	break;
+
     }
-    std::cout << back->val << "\n";	
 }
 
 int main()
-{
+{    
     Graph g {};
-    Eigen::MatrixXf n = Eigen::MatrixXf::Constant(3,3,1);
-    auto a = g.define(n);
-    Eigen::MatrixXf m(3,3);
-    m << 1,2,3,4,5,6,7,8,9;
-    auto b = g.define(m);
-    auto c = g.define(2);
-    auto d = add(a, multiply(b, c));
-    std::cout << "A: " << a << "\n";
-    std::cout << "B: " << b << "\n";
-    std::cout << "C: " << c << "\n";
-    std::cout << "D: " << d << "\n\n";
-    g.eval(d);
-    std::cout << d->val << '\n';
+
+    // Define input biases
+    Eigen::MatrixXf _b = Eigen::MatrixXf::Constant(5,3,0);
+    auto b = g.define(_b);
+
+    // Define input layer
+    Eigen::MatrixXf _x(5,3);
+    _x << 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15;
+    auto x = g.define(_x);
+
+    // Define weights
+    Eigen::MatrixXf _W = Eigen::MatrixXf::Constant(3,4,1);
+    auto W = g.define(_W);
+
+    // Define hidden layer output
+    auto h =add(b, multiply(W, x));
+    std::cout << "HIDDEN LAYER\n" << h->val << "\n\n";
+
+    // Define final weights
+    Eigen::MatrixXf _V = Eigen::MatrixXf::Constant(4,2,1);
+    auto V = g.define(_V);
+
+    // Define final biases
+    Eigen::MatrixXf _a = Eigen::MatrixXf::Constant(5,2,0);
+    auto a = g.define(_a);
+
+    // Define output layer
+    auto y = add(a, multiply(V, h));
+
+    // Feedforward.
+    g.eval(y);
+    
+    // Get output.
+    std::cout << y->val << '\n';
 }
