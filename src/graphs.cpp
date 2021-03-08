7cb0de05e27752bfafc7fdc6ea4238fa7ad6f309
David Freifeld <freifeld.david@gmail.com>
2021-03-07 16:05:57 -0800

    Fix graph construction code

--- src/graphs.cpp
+++ src/graphs.cpp
@@ -1,44 +1,70 @@
 #include <functional>
 #include <vector>
 
-enum class Operation {add, multiply};
+enum class Operation {NONE, add, multiply};
     
-class Node
-{
+struct Node
+{    
     Operation op;
     float value;
-    std::vector<Node*> children;
+    std::vector<Node*> args;
+    Node(float v);
+    Node(Operation o, std::vector<Node*> a);
 };
+
+Node::Node(float v) : op(Operation::NONE), value(v), args{} {}
+Node::Node(Operation o, std::vector<Node*> a) : op(o), value(), args(a) {}
 
 Node* add(Node* a, Node* b)
 {
-    Node* result = new Node;
-    a->children.push_back(result);
-    b->children.push_back(result);
+    Node* result = new Node(Operation::add, {a,b});
+    result->args.push_back(a);
+    result->args.push_back(b);
     result->op = Operation::add;
     return result;
+    
 }
 
 Node* multiply(Node* a, Node* b)
 {
-    Node* result = new Node;
-    a->children.push_back(result);
-    b->children.push_back(result);
+    Node* result = new Node(Operation::multiply, {a,b});
+    result->args.push_back(a);
+    result->args.push_back(b);
     result->op = Operation::multiply;
     return result;
+}
+
+class Graph
+{
+    std::vector<Node> input;
+public:
+    Graph();
+    Node* define(float a);
+    void start(Node* back);
 };
 
-Node* define(float a)
+Node* Graph::define(float a)
 {
-    Node* result = new Node;
-    result->value = a;
+    input.emplace_back(a);
+    return &input[input.size()-1];
+}
+
+
+
+void Graph::start(Node* back)
+{
+    Node* cur = back;
+    for (size_t i = 0; i < back->args.size(); i++) {
+	
+    }
 }
 
 int main()
 {
-    auto a = define(5);
-    auto b = define(3.2);
-    auto c = define(2);
+    Graph g {};
+    auto a = g.define(5);
+    auto b = g.define(3.2);
+    auto c = g.define(2);
     auto d = add(a, multiply(b, c));
-    
+    g.start(d);
 }
